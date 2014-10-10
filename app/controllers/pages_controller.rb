6860be31e8214737ab46b34c3052406084d2afbb
Evan Moore <etmoore@gmail.com>
2014-10-10 09:29:36 -0600

    added pg and rails_12factor for Heroku

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,32 @@ class PagesController < ApplicationController
 
   def index
     render :index
+    columns
   end
 
+  def columns
+    @columns = [
+      {
+        title: 'Tasks',
+        content: [
+          "Grouped by projects and lists. Just the way you like'em"
+        ]
+      },
+      {
+        title: 'Documents',
+        content: [
+          'Upload',
+          'Comment',
+          'Revise'
+        ]
+      },
+      {
+        title: 'Comments',
+        content: [
+          'Comment on tasks and documents',
+          'Get email notifications'
+        ]
+      }
+    ]
+  end
 end

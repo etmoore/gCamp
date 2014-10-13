f311a6d1e32b41ecdbc26314111885885e3764f4
Evan Moore <etmoore@gmail.com>
2014-10-13 15:04:59 -0600

    added links to footer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,19 @@
 class PagesController < ApplicationController
 
-  def index
+  def homepage
     @columns = {Tasks:['Grouped by projects and lists, just the way you like\'em'],
                 Documents:['Upload','Comment','Revise'],
                 Comments:['Comment on task and documents','Get email notifications']
                 }
     render :homepage
   end
+
+  def about
+    render :about
+  end
+
+  def terms
+    render :terms
+  end
+
 end

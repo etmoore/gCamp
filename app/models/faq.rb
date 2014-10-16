7c4da8634e780e90c424ee35f95d99384859d262
Evan Moore <etmoore@gmail.com>
2014-10-16 11:15:02 -0600

    disconnected Faq.all from controller to prevent duplication of faqs

--- app/models/faq.rb
+++ app/models/faq.rb
@@ -1,5 +1,6 @@
 class Faq
 
+  # FIXTHIS keeps track of all instances ever created, will result in duplicate faqs
   def self.all
     ObjectSpace.each_object(self).to_a
   end
@@ -9,6 +10,7 @@ class Faq
   def initialize(question, answer)
     self.question = question
     self.answer = answer
+
   end
 
 end

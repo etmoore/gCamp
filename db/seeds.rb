2d7ad8e400f2247937bb17fa61ce905745930eee
Evan Moore <etmoore@gmail.com>
2014-10-23 15:33:42 -0600

    seeding tasks with faker data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Task.delete_all
+
+30.times do
+  Task.create description: Faker::Lorem.sentence,
+              complete: [true, false, false, false].sample,
+              due: Faker::Time.forward(21)
+end

d9ed265a39405394e6afd7248f9fc29dbf2a6fe3
Evan Moore <etmoore@gmail.com>
2014-10-27 13:59:00 -0600

    set up model, view, and controllers for users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params.require(:user).permit(:first_name, :last_name, :email))
+    if @user.save
+      redirect_to users_path, notice: "User successfully created."
+    else
+      render "new"
+    end
+  end
+end

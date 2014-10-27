45c602e822a5486e23ca10ef10f1ee0f23fb7712
Evan Moore <etmoore@gmail.com>
2014-10-27 14:11:49 -0600

    seeding database with fake users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
 Task.delete_all
+User.delete_all
 
 30.times do
   Task.create description: Faker::Lorem.sentence,
               complete: [true, false, false, false].sample,
               due: Faker::Time.forward(21)
+end
+
+30.times do
+  first_name = Faker::Name.first_name
+  User.create first_name: first_name,
+              last_name: Faker::Name.last_name,
+              email: Faker::Internet.safe_email(first_name)
+
 end

2daa6efbf2c6a9d3d5dff91181440843cdf7feed
Evan Moore <etmoore@gmail.com>
2014-10-28 10:01:15 -0600

    edit page for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def index
     @users = User.all
   end
@@ -7,12 +9,37 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def create
-    @user = User.new(params.require(:user).permit(:first_name, :last_name, :email))
+    @user = User.new(user_params)
     if @user.save
       redirect_to users_path, notice: "User successfully created."
     else
       render "new"
     end
   end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to users_path, notice: 'User was successfully updated.' }
+      else
+        render "edit"
+      end
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email)
+  end
+
 end

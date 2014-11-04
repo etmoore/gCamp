f50b3586643592d45afe63034e3c8bf90865712b
Evan Moore <etmoore@gmail.com>
2014-11-04 14:04:00 -0700

    added registration and authentication routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,10 @@ Rails.application.routes.draw do
   get "about" => "about#show", as: :about
   get "terms" => "terms#show", as: :terms
   get "faq" => "faq#show", as: :faq
+  get '/sign-up' => 'registrations#new', as: :signup
+  post '/sign-up' => 'registrations#create'
+  get '/sign-in' => 'authentication#new', as: :signin
+  post '/sign-in' => 'authentication#create'
+  get '/sign-out' => 'authentication#destroy', as: :signout
 
 end

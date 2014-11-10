ac538538047ef5cdeda29df159b8f613a29c4437
Evan Moore <etmoore@gmail.com>
2014-11-10 15:56:29 -0700

    test for duplicate emails

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -9,4 +9,21 @@ feature "Users" do
     expect(page).to have_content "Last name can't be blank"
     expect(page).to have_content "Email can't be blank"
   end
+
+  scenario "user attempts to create a new user with duplicate email" do
+    User.create first_name: "Joe",
+                last_name: "Schmoe",
+                email: "jschmoe@example.com",
+                password: "test",
+                password_confirmation: "test"
+    visit users_path
+    click_on "New User"
+    fill_in "First name", with: "Bob"
+    fill_in "Last name", with: "Cookabara"
+    fill_in "Email", with: "jschmoe@example.com"
+    fill_in "Password", with: 'testy'
+    fill_in "Password confirmation", with: 'testy'
+    click_on "Create User"
+    expect(page).to have_content "Email has already been taken"
+  end
 end

3d78474146b29805a2b7834285fa83b7e946e9af
Evan Moore <etmoore@gmail.com>
2014-11-11 17:20:36 -0700

    moved method to private

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,17 +3,20 @@ class Task < ActiveRecord::Base
   validates :description, presence: true
   validate :not_past_due
 
-  def due_soon?
-    self.due.present? && self.due <= (Date.today + 7)
-  end
-
-  def past_due_date?
-    self.due < Date.today
-  end
-
   def not_past_due
     if self.due.present? && past_due_date?
       errors.add(:due, 'date has already passed')
     end
   end
+
+  def due_soon?
+    self.due.present? && self.due <= (Date.today + 7)
+  end
+
+  private
+
+    def past_due_date?
+      self.due < Date.today
+    end
+
 end

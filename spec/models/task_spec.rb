df67fd7e99c97720d87829c4a223c0fe7b479f79
Evan Moore <etmoore@gmail.com>
2014-11-14 13:07:56 -0700

    test for task date validation

---
+++ spec/models/task_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe Task do
+  it "validates that the due date is not in the past" do
+    task = Task.new description: 'past task', due: Date.today - 1
+    task.valid?
+    expect(task.errors[:due].present?).to eq(true)
+  end
+end

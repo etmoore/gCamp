6fb331a0e9458486514235473fabb81ac84614ed
Evan Moore <etmoore@gmail.com>
2014-11-14 12:54:39 -0700

    test for editing a project

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -9,6 +9,15 @@ feature "Projects" do
     expect(page).to have_content "Take over the world"
   end
 
+  scenario "User edits a project" do
+    Project.create name: "woo!"
+    visit projects_path
+    click_on "Edit"
+    fill_in "Name", with: "woo two!"
+    click_on "Update Project"
+    expect(page).to have_content "woo two!"
+  end
+
   scenario "User attempts to create a project without a name" do
     visit projects_path
     click_on "Create Project"

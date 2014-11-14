dcdd84718c2f3e89856013f50e0b384ebae4489b
Evan Moore <etmoore@gmail.com>
2014-11-14 12:52:19 -0700

    test for creating a project

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 feature "Projects" do
+  scenario "User creates a project" do
+    visit projects_path
+    click_on "Create Project"
+    fill_in "Name", with: "Take over the world"
+    click_on "Create Project"
+    expect(page).to have_content "Take over the world"
+  end
+
   scenario "User attempts to create a project without a name" do
     visit projects_path
     click_on "Create Project"

99d2c4420cb995cca8d7d62151322268bcb9b2e0
Evan Moore <etmoore@gmail.com>
2014-11-23 00:31:16 -0700

    users can change the role of project members

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,28 +1,42 @@
 class MembershipsController < ApplicationController
   before_action :set_project
+
   def index
     @membership = Membership.new
     @memberships = @project.memberships
   end
 
   def create
-    @membership = @project.memberships.new(params.require(:membership).permit(:role, :user_id))
-    if @membership.save
-      redirect_to project_memberships_path(@project), notice: "#{@membership.user.full_name} added successfully"
+    membership = @project.memberships.new(membership_params)
+    if membership.save
+      redirect_to project_memberships_path, notice: "#{membership.user.full_name} was added successfully"
+    else
+      render :index
+    end
+  end
+
+  def update
+    membership = @project.memberships.find(params[:id])
+    if membership.update(membership_params)
+      redirect_to project_memberships_path, notice: "#{membership.user.full_name} was updated successfully"
     else
       render :index
     end
   end
 
   def destroy
-    @membership = @project.memberships.find(params[:id])
-    @membership.destroy
-    redirect_to project_memberships_path, notice: "#{@membership.user.full_name} was destroyed successfully"
+    membership = @project.memberships.find(params[:id])
+    membership.destroy
+    redirect_to project_memberships_path, notice: "#{membership.user.full_name} was destroyed successfully"
   end
 
   private
 
     def set_project
       @project = Project.find(params[:project_id])
+    end
+
+    def membership_params
+      params.require(:membership).permit(:role, :user_id)
     end
 end

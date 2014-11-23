83dd0a351159a2547e68c44b6bfcfda820d951dd
Evan Moore <etmoore@gmail.com>
2014-11-23 00:21:42 -0700

    seeds users with passwords

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,12 @@
-Task.delete_all
 User.delete_all
-
-# 100.times do
-#   Task.create description: Faker::Lorem.sentence,
-#               complete: [true, false, false, false].sample,
-#               due: Faker::Time.forward(21)
-# end
 
 30.times do
   first_name = Faker::Name.first_name
+  password = Faker::Internet.password
   User.create first_name: first_name,
               last_name: Faker::Name.last_name,
-              email: Faker::Internet.safe_email(first_name)
+              email: Faker::Internet.safe_email(first_name),
+              password: password,
+              password_confirmation: password
 
 end

6f48f84fdf5d712b185e7e45e9f01808be1569a5
Evan Moore <etmoore@gmail.com>
2014-12-01 22:21:13 -0700

    tests for adding and deleting a membership

--- spec/features/memberships_spec.rb
+++ spec/features/memberships_spec.rb
@@ -1,7 +1,26 @@
 require 'rails_helper'
 
 feature 'Memberships' do
-  scenario 'User deletes a membership' do
+  scenario 'User creates a membership' do
+    user = create_user
+    project = create_project
+    visit project_memberships_path(project)
+    select user.full_name, from: "membership_user_id"
+    select "Member", from: "membership_role"
+    click_on "Add New Member"
+    expect(page).to have_content user.full_name
   end
 
+  scenario 'User deletes a membership' do
+    user = create_user
+    project = create_project
+    visit project_memberships_path(project)
+    select user.full_name, from: "membership_user_id"
+    select "Member", from: "membership_role"
+    click_on "Add New Member"
+    expect(page).to have_content user.full_name
+
+    find(".glyphicon-remove").click
+    expect(page).to have_content "#{user.full_name} was removed successfully"
+  end
 end

05bb02fc5a42b8fc5342420a0a7a50830380f221
Evan Moore <etmoore@gmail.com>
2014-12-03 11:10:13 -0700

    refactored user spec and added test for email validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,18 +2,27 @@ require 'rails_helper'
 
 describe User do
   it "Validates email uniqueness" do
-    user = User.create first_name: 'Aaron', last_name: 'Rogers', email: 'arod@arod.com',
-                        password: 'arod', password_confirmation: 'arod'
-    dup_user = User.create first_name: 'Aaron', last_name: 'Rogers', email: 'arod@arod.com',
-                        password: 'arod', password_confirmation: 'arod'
+    user = create_user email: 'arod@arod.com'
+    dup_user = new_user email: 'arod@arod.com'
     dup_user.valid?
     expect(dup_user.errors[:email].present?).to eq(true)
   end
 
-  it "Validates first_name and last_Name presence" do
-    user = User.new
+  it "Validates email presence" do
+    user = new_user email: nil
+    user.valid?
+    expect(user.errors[:email].present?).to eq(true)
+  end
+
+  it "Validates first_name presence" do
+    user = new_user first_name: nil
     user.valid?
     expect(user.errors[:first_name].present?).to eq(true)
+  end
+
+  it "Validates last_name presence" do
+    user = new_user last_name: nil
+    user.valid?
     expect(user.errors[:last_name].present?).to eq(true)
   end
 

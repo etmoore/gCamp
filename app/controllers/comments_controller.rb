57b16fd493e2c543305ba0093f979b1befa0652c
Evan Moore <etmoore@gmail.com>
2014-12-03 13:43:46 -0700

    refactored comment controller and task view to no longer use hidden project_id field

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,12 @@
 class CommentsController < ApplicationController
   before_action do
-    @project = Project.find(params[:comment][:project_id])
     @task = Task.find(params[:comment][:task_id])
   end
 
   def create
     @comment = Comment.new(params.require(:comment).permit(:comment, :task_id))
     @comment.user_id = current_user.id
-    if @comment.save
-      redirect_to project_task_path(@project, @task)
-    else
-      render project_task_path
-    end
+    @comment.save
+    redirect_to project_task_path(@task.project, @task)
   end
 end

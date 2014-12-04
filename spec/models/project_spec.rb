2a80de420a8eb0b6cab7ff6a8bd8752e38ae33f1
Evan Moore <etmoore@gmail.com>
2014-12-04 16:17:09 -0700

    When a user deletes a project all related data is deleted

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -5,4 +5,21 @@ describe Project do
     project = new_project name: nil
     expect(project.valid?).to eq(false)
   end
+
+  it "destroys associated models (memberships, tasks, comments) when it gets destroyed" do
+    project = create_project
+    membership = create_membership project: project
+    task = create_task project: project
+    comment = create_comment task: task, user: membership.user
+
+    expect(project.memberships.count).to eq(1)
+    expect(project.tasks.count).to eq(1)
+    expect(task.comments.count).to eq(1)
+
+    project.destroy
+    expect(project.memberships.count).to eq(0)
+    expect(project.tasks.count).to eq(0)
+    expect(task.comments.count).to eq(0)
+
+  end
 end

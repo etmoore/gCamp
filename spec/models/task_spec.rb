e9e9a8092fd46e0aecaff3feff7069380bb1ca42
Evan Moore <etmoore@gmail.com>
2014-12-05 15:39:44 -0700

    destroying task destroys associated comments

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,14 +2,23 @@ require 'rails_helper'
 
 describe Task do
   it "validates that the due date is not in the past" do
-    task = Task.new description: 'past task', due: Date.today - 1
+    task = new_task due: Date.today - 1
     task.valid?
     expect(task.errors[:due].present?).to eq(true)
   end
 
   it "validates that the task has a description" do
-    task = Task.new
+    task = new_task description: nil
     task.valid?
     expect(task.errors[:description].present?).to eq(true)
   end
+
+  it "destroys related comments when destroyed" do
+    task = create_task
+    comment = create_comment task: task, user: new_user
+    expect(task.comments.count).to eq(1)
+    task.destroy
+    expect(Comment.all.count).to eq(0)
+  end
+
 end

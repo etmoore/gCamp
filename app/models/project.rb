46b1887fa1367942b31aa9842b7f0efe86922d99
Evan Moore <etmoore@gmail.com>
2014-12-06 20:39:53 -0700

    adds relationship between users and projects through memberships

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,5 @@ class Project < ActiveRecord::Base
   validates :name, presence: true
   has_many :tasks, dependent: :destroy
   has_many :memberships, dependent: :destroy
+  has_many :users, through: :memberships
 end

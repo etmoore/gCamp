a506c8483f2c0697412d575c001ca3ff151fbd31
Evan Moore <etmoore@gmail.com>
2014-12-06 21:26:26 -0700

    authentication controller spec

--- spec/controllers/authentication_controller_spec.rb
+++ spec/controllers/authentication_controller_spec.rb
@@ -2,10 +2,20 @@ require 'rails_helper'
 
 describe AuthenticationController do
   describe '#create' do
-    it 'sets the session :user_id and redirects to the projects index'
+    it 'sets the session[:user_id] and redirects to the projects index' do
+      user = create_user
+      params = {authentication: {email: user.email, password: user.password}}
+      post :create, params
+      expect(session[:user_id]).to eq(user.id)
+      expect(response).to redirect_to(projects_path)
+    end
   end
 
   describe '#destroy' do
-    it 'clears the session object and redirects to the root path'
+    it 'clears the session object and redirects to the root path' do
+      session[:user_id] = 3
+      get :destroy
+      expect(session.empty?).to eq(true)
+    end
   end
 end

cf641b5842894f55552d0c8d505139e1ef8f6dcf
Evan Moore <etmoore@gmail.com>
2014-12-14 20:47:04 -0700

    added owner? method

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -4,4 +4,8 @@ class Membership < ActiveRecord::Base
 
   validates :role, presence: true
   validates :user, presence: true, uniqueness: {scope: :project, message: "has already been added to project"}
+
+  def owner?
+    self.role == "owner"
+  end
 end

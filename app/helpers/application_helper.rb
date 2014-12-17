ded318ecb3637a1ce8754a4501fa4ea1f430435f
Evan Moore <etmoore@gmail.com>
2014-12-17 11:32:48 -0700

    page header helper and content_for(title)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+  def page_header(title)
+    content_for(:title, title)
+    html = '<div class="page-header">'.html_safe
+    if block_given?
+      html += '<div class="pull-right">'.html_safe
+      html += capture { yield }
+      html += '</div>'.html_safe
+    end
+    html += '<h1>'.html_safe
+    html += title
+    html += '</h1>'.html_safe
+    html += '</div>'.html_safe
+    html
+  end
+
 end
